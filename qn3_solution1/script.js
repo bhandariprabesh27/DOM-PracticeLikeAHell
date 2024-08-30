9c2fecc970dde7929e31f1530b4dbe96dc49339d
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-08-30 12:00:10 +0545

    answer completed

--- qn3_solution1/script.js
+++ qn3_solution1/script.js
@@ -3,3 +3,19 @@
 //Jaba hami forms snga deal garda submit garxam taba page reload hunxa
 //hamile khyal rakhnu parxa ki page reload hunuhudaina
 //yedi page reload vayo vane JS chaldaina, kinaki JS chalnu vanda paila nai page reload vaisakeko hunxa
+
+var selctForm = document.querySelector("form");
+var input1 = document.querySelector("#inp1");
+var input2 = document.querySelector("#inp2");
+var selecth3 = document.querySelector("h3");
+
+selctForm.addEventListener("submit", function (ev) {
+  ev.preventDefault(); // yesle page refresh huna rokxa
+  if (input1.value === "" || input2.value === "") {
+    selecth3.textContent = "Error: Some fields are blank!";
+    selecth3.style.color = "red";
+  } else {
+    selecth3.textContent = "";
+    selecth3.style.color = "black";
+  }
+});
