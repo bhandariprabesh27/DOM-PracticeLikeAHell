de57b7ce88077d706e287d4181c6f7aa3abc8704
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-08-30 23:19:41 +0545

    initial commit 0

---
+++ qn5/script.js
@@ -0,0 +1,5 @@
+//Qn. Build a countdown timer that starts when a button is clicked and updates the display in real time
+var btn = document.querySelector("button");
+var heading1 = document.querySelector("h1");
+
+btn.addEventListener("click", function () {});
