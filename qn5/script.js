436960b8ab28af90e09f8980a5fe6a4b675745c6
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-08-30 23:54:20 +0545

    answer completed

--- qn5/script.js
+++ qn5/script.js
@@ -1,5 +1,16 @@
 //Qn. Build a countdown timer that starts when a button is clicked and updates the display in real time
-var btn = document.querySelector("button");
 var heading1 = document.querySelector("h1");
+var srt = document.querySelector("#start");
+var stp = document.querySelector("#stop");
 
-btn.addEventListener("click", function () {});
+var intrvl;
+srt.addEventListener("click", function () {
+  var count = 0;
+  intrvl = setInterval(function () {
+    count++;
+    heading1.textContent = count;
+  }, 1000);
+});
+stp.addEventListener("click", function () {
+  clearInterval(intrvl);
+});
