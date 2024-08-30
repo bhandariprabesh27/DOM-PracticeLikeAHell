be1a96355a46d352ef53ff6a25f7b14852eaf3dd
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-08-30 14:10:28 +0545

    answer completed

--- qn3_solution2/script.js
+++ qn3_solution2/script.js
@@ -1,2 +1,20 @@
 //Qn: Create a form with input fields and submit button. Use Javascript to validate the form and display an error message if the input is invalid.
 //Solve this question using loop.
+
+var selectForm = document.querySelector("form");
+var inps = document.querySelectorAll('input[type="text"]');
+var selecth3 = document.querySelector("h3");
+
+selectForm.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  for (var i = 0; i < inps.length; i++) {
+    if (inps[i].value.trim() === "") {
+      selecth3.textContent = "Error: Some fields are empty";
+      selecth3.style.color = "red";
+      break;
+    } else {
+      selecth3.textContent = "";
+      selecth3.style.color = "black";
+    }
+  }
+});
