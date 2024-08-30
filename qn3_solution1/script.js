1e2c26753f3f072422518986fe14d9010e632b76
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-08-30 11:37:48 +0545

    initial commit 01

---
+++ qn3_solution1/script.js
@@ -0,0 +1,5 @@
+//Qn: Create a form with input fields and submit button. Use Javascript to validate the form and display an error message if the input is invalid.
+
+//Jaba hami forms snga deal garda submit garxam taba page reload hunxa
+//hamile khyal rakhnu parxa ki page reload hunuhudaina
+//yedi page reload vayo vane JS chaldaina, kinaki JS chalnu vanda paila nai page reload vaisakeko hunxa
