cd535d6066c894dced98a5b42d68d35af8ee1a21
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-01 17:14:05 +0545

    answer completed

--- qn7/script.js
+++ qn7/script.js
@@ -1,1 +1,13 @@
 //Display a progress bar that updates in real time, showing the progress of a task, download or form submission.
+
+var pgr = document.querySelector("#progress");
+var selecth3 = document.querySelector("h3");
+var count = 0;
+var intrvl = setInterval(function () {
+  if (count === 100) {
+    selecth3.style.opacity = "1";
+    clearInterval(int);
+  }
+  count++;
+  pgr.style.width = count + "%";
+}, 50);
