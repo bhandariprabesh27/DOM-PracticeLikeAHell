1c7a198278734adfaeee1e7811b692383acbb3e3
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-01 06:00:46 +0545

    final commit

--- qn6_solution2/script.js
+++ qn6_solution2/script.js
@@ -1,1 +1,19 @@
 //Create a tabbed interface where clicking on tabs displays different content sections without page reload.
+var divs = document.querySelectorAll(".tab");
+var texts = document.querySelectorAll("h3");
+
+texts[0].style.display = "block";
+texts[0].style.width = "50%";
+
+divs.forEach(function (div, index) {
+  div.addEventListener("click", function () {
+    hideAllText();
+    texts[index].style.display = "block";
+    texts[index].style.width = "50%";
+  });
+});
+function hideAllText() {
+  texts.forEach(function (text) {
+    text.style.display = "none";
+  });
+}
