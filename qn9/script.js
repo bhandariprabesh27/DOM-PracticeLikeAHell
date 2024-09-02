bb7f92322f3fd6b28b8e43c95cfd0a18fdd2ffe2
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-02 09:24:52 +0545

    final commit

--- qn9/script.js
+++ qn9/script.js
@@ -1,1 +1,8 @@
 //Build a character counter for a text area or input field which updates in real time as the user types and enforces a character limit.
+
+var txtarea = document.querySelector("textarea");
+var counterselect = document.querySelector("#counter");
+
+txtarea.addEventListener("input", function () {
+  counterselect.textContent = txtarea.value.length;
+});
